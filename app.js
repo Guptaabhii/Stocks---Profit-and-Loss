af05a2772990ed85e9d963b5b285696316eda69a
Guptaabhii <112747867+Guptaabhii@users.noreply.github.com>
2022-09-11 23:00:23 +0530

    completed bonus part

--- app.js
+++ app.js
@@ -9,30 +9,45 @@ function submitHandler(){
     const ip = Number(initialPrice.value);
     const curr = Number(currentPrice.value);
     const sq = Number(stockQuantity.value);
-    calculateProfitAndLoss(ip, sq, curr);
+    checkValue(ip, sq, curr);
 }
 
  function calculateProfitAndLoss(initial, quantity, current){
     if(initial>current){
         const loss = (initial - current)*quantity;
         const lossPercentage = (loss/initial)*100;
-        showMessage("Oops! your loss is "+loss+ "and loss percentage is "+
+        document.getElementById("output").style.color="red";
+        showMessage("Oops! your loss is "+loss+ " and loss percentage is "+
         dotTwoLimiter(lossPercentage)+"%");
 
     }else if(current>initial){
         const profit = (current - initial)*quantity;
         const profitPercentage = (profit/initial)*100;
-        showMessage("great! your profit is "+profit+"and profit percentage is "
+        document.getElementById("output").style.color="green";
+        showMessage("great! your profit is "+profit+" and profit percentage is "
         +dotTwoLimiter(profitPercentage)+"%");
     }
     else{
+        document.getElementById("output").style.color="blue";
             showMessage("no profit, no loss");
     }
 
 }
 function showMessage(message){
-    output.innertext = message;
+    output.innerText = message;
 }
 function dotTwoLimiter(y){
     return Number.parseFloat(y).toFixed(2);
+}
+function checkValue(ip, sq, curr){
+    if(ip === 0 && sq===0 && curr===0){
+        document.getElementById("output").style.color="red";
+        showMessage("nothing is entered");
+    }else if(ip === 0 || sq===0 || curr===0){
+        document.getElementById("output").style.color="orange";
+        showMessage("some fields are still empty");
+    }
+    else{
+        calculateProfitAndLoss(ip, sq, curr);
+    }
 }
